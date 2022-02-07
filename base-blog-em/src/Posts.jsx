42721feb2d330dd0cbe114cb35c4e927de98decb
MACHE022 <edson.machado.-nd@disney.com>
2022-02-07 19:53:40 +0100

    Handling loading & error states

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -15,9 +15,17 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data } = useQuery('posts', fetchPosts);
+  const { data, isError, error, isLoading } = useQuery('posts', fetchPosts);
 
-  if (!data) return <div>Loading...</div>;
+  if (isLoading) return <h3>Loading...</h3>;
+
+  if (isError)
+    return (
+      <>
+        <h3>Oops, something went wrong!</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
